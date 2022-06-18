fb9b30a3ceb2314f9ceb3c72145b5e7b6661c940
kamal <kamade123@gmail.com>
2022-06-18 13:21:07 -0300

    server setup complete, home  page added

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+
+function onRequest(req, res) {
+    res.writeHead(200, {'Content-Type':'text/html'});
+    fs.readFile('./index.html', null,(err, data)=>{
+        if(err) {
+            res.writeHead(404);
+            res.write('file not found')
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
+}
+
+http.createServer(onRequest).listen(8000,()=>{
+    console.log('server up and runing')
+});
